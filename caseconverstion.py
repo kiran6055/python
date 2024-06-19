e8e418625ff736a3c4aae1331bc4dfb8e5217067
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-06-19 17:17:58 +0530

    Update caseconverstion.py

--- caseconverstion.py
+++ caseconverstion.py
@@ -1,1 +1,4 @@
-
+my_self="kirankumar learning python for devops"
+print(my_self)
+print(my_self.swapcase())
+# here we can change the string from upper to lower lower to upper this result only in print statement but it will not change the string variable
