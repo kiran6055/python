23ca37cf66bf59391212ccd1e9050d1231150c80
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-06-19 16:07:31 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,27 @@
+my_name="kiran"
+my_last_job="previosuly wokred as recruiter"
+my_job="""looking for
+DevopsAWS enginner role
+in any of the good compnay"""
+#by giving """ 3 quotes we can give string down side also it will take a a string by giving duble quotes " it will not work by giving ' single quotes also witll not works
+
+my_new_job=f"{my_last_job}" # if we dont give here below print with len we will use that print
+#my_new_job=len(f"{my_last_job}") #if we give len here we use belwo first print with out len
+
+print(f" adding two variables my_name and my_lastjob: {my_name} {my_last_job}") # like this also we can add or we can use concat like below
+print(f"  adding two variables my_name and my_lastjob: {my_name + my_last_job}") # this is how we give concation between two string varaibles
+
+
+#print(my_new_job)
+#print(f"the length of mynewjob: {my_new_job}")
+#print(f"the length of the mynewjob: {len(my_last_job)}")
+#print(my_last_job[0:10])
+
+#print(f" the words from 11 to 25 will see here this we can call it is as slice function also: {my_new_job[10:25]}")
+
+#print(f'{my_job} \n {my_last_job} \n {my_job}') #n is used to print the next variable or string from next line
+#print(my_job)
+#print(my_name)
+#print(my_last_job)
+
+#by giving """ 3 quotes we can give string down side also it will take a a string by giving duble quotes " it will not work by giving ' single quotes also witll not works
