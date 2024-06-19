6581efd05134c1fe18331d9edefa98245f52a747
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-06-19 17:02:35 +0530

    Create boolean.py

---
+++ boolean.py
@@ -0,0 +1,9 @@
+in boolean we have to give True (capital T) if we give small t it will throw error same for False also first letter should be capital not samll f
+
+  my_course="studying python"
+
+print(my_course.startswith('z'))
+# here we are checking for boolean True or False my_course in print is string variables .startswith is keyword wich we use('z') False will come
+print(my_course.endswith('kl'))
+print(my_course.islower())
+# if we are checking entire string we use this()
