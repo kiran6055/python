8f31e84453ad3f93e527bb2c757bd8dc450f2689
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-06-19 12:32:02 +0530

    Create ex.py

---
+++ ex.py
@@ -0,0 +1,31 @@
+x=3;y=4.6;z="1+k"
+
+
+
+print(x,type(x),(y),type(y),z,type(z))
+
+
+my_name='kiran'
+print(my_name,type(my_name))
+
+my_value=True
+print(my_value)
+print(type(my_value))
+
+
+#changing data types
+A=-5
+print(A,type(A))
+B=str(A)
+print(B)
+print(type(B))
+C=bool(A)
+print(C,type(C))
+
+#here we have given varaibales x as float but we are printing only integer
+x=10.3090
+print(int(x))
+y=bool(x)
+print(y,type(y))
+
+
