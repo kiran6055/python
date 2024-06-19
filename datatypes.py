9408eef612d34ddc88cc18fe4da245c044e0d57a
kiran6055 <133787313+kiran6055@users.noreply.github.com>
2024-06-19 12:30:34 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+if any variables with start with number 100 or 1 or 100000 with out any faction it is called as integer
+x=100
+print(x,type(x))
+
+if any number having faction we call it as float
+y=10.3456
+print(y,type(y))
+
+if ny datatype represents inside quotes we call it as string
+my_name="my_name is kiran you all know that"
+print(my_name,type(my_name))
+
+we can convert datatypes 
+
+we can convert any data type into boolean
+boolean of any datatype is = True or False
+boolean(empty) = False
+boolean(non-empty)=Ture
+this is if a value is holding 0 zeero it will be false it any values is holdeing other than 0 it will be true from -1 to infinity
+  x=""
+  bool(x) bool([])this is list,bool(()) this is tuple, bool({}) this is dictonary
+   bool(none) these thing will be false only
+   if we dont provide any thing it will be false only
+   we can convert data types also from integer to string integer to boolean but we can't convert string to boolean
+
+Any data type can be converted into string but the reversal is not alwways true number can be convert into string but the alphabeets can'bt be converted
